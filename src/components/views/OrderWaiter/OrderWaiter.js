dfdba84741a734c459dc43895011f04fee5994c1
Paweł Jankowski <paweljankowski92@gmail.com>
2021-02-23 15:37:40 +0100

    add nav and views

--- src/components/views/OrderWaiter/OrderWaiter.js
+++ src/components/views/OrderWaiter/OrderWaiter.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderWaiter.scss';
+import { Link } from 'react-router-dom';
 
 const OrderWaiter = () => (
   <div className={styles.component}>
     <h2>OrderWaiter view</h2>
+      <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Link>
+      <Link to={`${process.env.PUBLIC_URL}/waiter/:id`}> order id</Link>
   </div>
 );
 
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Tables.scss';
+import { Link } from 'react-router-dom';
 
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
+      <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>new booking</Link>
+      <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}> booking id</Link>
+      <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>new event</Link>
+      <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`}> event id</Link>
   </div>
 );
 
